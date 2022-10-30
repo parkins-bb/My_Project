cbaa6e8f9c877796a5b25c98d76b62fbe4133e11
zhaixiuxi <59047218+parkins-bb@users.noreply.github.com>
2022-10-30 22:53:16 +0800

    first commit

---
+++ C_learning_1/C_learning_1.cpp
@@ -0,0 +1,10 @@
+﻿#include <stdio.h>
+#include <windows.h>
+int main() {
+    int a = 0100;
+    int b = 100;
+    int c = 0X100;
+    printf("十进制为：" "a=%d, b=%d, c=%d\n", a, b, c);
+    printf("各自进制为：" "a=%#o, b=%d, c=%#X", a, b, c);
+    return 0;
+}
